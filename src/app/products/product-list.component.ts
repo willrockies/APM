ebdc91fc96dfd6e06885de2e742c45855a6eb2a8
Wilson Alves <wlsn.ads@gmail.com>
2020-06-15 12:29:09 -0300

    services and dependency injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from '../interfaces/products';
+
+import { ProductService } from '../services/product.services';
 
 @Component({
   selector: 'pm-products',
@@ -25,32 +28,10 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png'
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2019',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png'
-    },
-  ];
-  constructor(){
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
-  }
+  products: IProduct[] = [];
+  constructor(
+    private productService: ProductService
+    ){}
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -67,6 +48,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('OnInit');
+    this.products = this.productService.getProduct();
+    this.filteredProducts = this.products;
   }
 }
